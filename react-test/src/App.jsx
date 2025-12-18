236425cc41f75d9228f87cec616135e1a95cf425
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-12-18 14:05:55 -0700

    Create basic hello page

---
+++ react-test/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div id="app-container" className="dark:bg-slate-900 dark:text-white min-h-screen">
+      <h1 className="text-center text-3xl p-[2rem]">Hello World!</h1>
+      
+      <h3 className="m-[0.75rem]">I don't know what to put here :/ </h3>
+    </div>
+  )
+}
+
+export default App
